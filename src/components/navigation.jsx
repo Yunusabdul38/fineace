2132180e08a66d3f716645ddbb2c4b68f4a4dc0d
ozo-vehe <johnnie.vehe@gmail.com>
2024-09-04 11:05:34 +0100

    new update

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,63 +1,73 @@
 import { CiMenuFries } from "react-icons/ci";
 import { HiXMark } from "react-icons/hi2";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Modal from "./Modal";
-import { CreatContext } from "../contex/WalletContextProvider";
+// import { CreatContext } from "../contex/WalletContextProvider";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useAccount } from "wagmi";
 
 export default function NavigationBar() {
   const [menu, setMenu] = useState(false);
- const {wallet} = useContext(CreatContext)
+  // const { wallet } = useContext(CreatContext)
+  const { address } = useAccount();
+
+  console.log(address);
+
   function menuHandler() {
     setMenu((prev) => !prev);
   }
-  const [isOpen,setIsOpen] = useState(false)
-  const modalHandler =()=>{
-    setIsOpen((prev)=>!prev)
+  const [isOpen, setIsOpen] = useState(false)
+  const modalHandler = () => {
+    setIsOpen((prev) => !prev)
   }
   return (
     <>
-    {isOpen&& <Modal modalHandler={modalHandler}/>}
-    <nav
-      className="w-full capitalize flex flex-col sm:flex-row justify-between sm:items-center border-b py-7 px-5 sm:pr-20 sm:h-20 lg:h-fit lg:pr-10 md:py-4 lg:px-10 fixed z-10 bg-white top-0 font-Urbanist"
-    >
-      <div
-        className="w-fit absolute top-10 sm:top-[30px] text-2xl right-4 sm:block lg:hidden"
-        onClick={menuHandler}
+      {isOpen && <Modal modalHandler={modalHandler} />}
+      <nav
+        className="w-full capitalize flex flex-col sm:flex-row justify-between sm:items-center border-b py-7 px-5 sm:pr-20 sm:h-20 lg:h-fit lg:pr-10 md:py-4 lg:px-10 fixed z-10 bg-white top-0 font-Urbanist"
       >
-        {!menu ? <CiMenuFries /> : <HiXMark />}
-      </div>
-      <div className="w-fit uppercase text-3xl font-bold bg-hero-2 bg-right-top text-orangeText  cursor-pointer">
-       <h3> fineace</h3>
-       <h5 className="text-xs font-thin text-gray-500">investment group</h5>
-      </div>
-      <ul
-        className={`flex sm:flex-row sm:absolute lg:relative sm:top-20 lg:top-0 sm:left-0 py-4 lg:py-0 sm:bg-white lg:bg-inherit sm:justify-around sm:w-full lg:w-fit ${
-          !menu ? "hidden" : "flex-col"
-        } lg:flex md:flex-row justify-between md:items-center gap-3 md:gap-5 w-fit`}
-        onClick={()=>setMenu(false)}
-      >
-        <NavLink to="/"><li>home</li></NavLink>
-        <NavLink to="/about-us" className="hover:text-[#FF450D]">
-          <li>about</li>
-        </NavLink>
-        <NavLink to="/contact-us" className="hover:text-[#FF450D]">
-        <li>contact us</li>
-        </NavLink>
-  
-      </ul>
-      <ul
-        className={`${
-          !menu ? "min-[360px]:absolute sm:relative min-[360px]:right-14 sm:right-0 max-[447px]:hidden" : ""
-        }  flex sm:flex-row justify-between sm:items-center gap-2 md:gap-5 w-fit`}
-      >
-        {!wallet && <button className="hover:bg-orangePrimary bg-white hover:text-white border-2 text-orangeText border-orangeText hover:border-transparent transition-all duration-500 ease-in-out rounded-md px-2 py-1 sm:px-7 sm:py-2 capitalize font-LexendDeca font-normal">connect wallet</button>}
-        {wallet && <>
-          <button className="hover:bg-orangePrimary bg-white hover:text-white border-2 text-orangeText border-orangeText hover:border-transparent transition-all duration-500 ease-in-out rounded-md px-2 py-1 sm:px-7 sm:py-2 capitalize font-LexendDeca font-normal" onClick={()=>{modalHandler()}}>create budget</button>
-          <button className="hover:bg-orangePrimary bg-white hover:text-white border-2 text-orangeText border-orangeText hover:border-transparent transition-all duration-500 ease-in-out rounded-md px-2 py-1 sm:px-7 sm:py-2 capitalize font-LexendDeca font-normal">0x00...789</button>
-        </>}
-      </ul>
-    </nav>
+        <div
+          className="w-fit absolute top-10 sm:top-[30px] text-2xl right-4 sm:block lg:hidden"
+          onClick={menuHandler}
+        >
+          {!menu ? <CiMenuFries /> : <HiXMark />}
+        </div>
+        <div className="w-fit uppercase text-3xl font-bold bg-hero-2 bg-right-top text-orangeText  cursor-pointer">
+          <h3> fineace</h3>
+          <h5 className="text-xs font-thin text-gray-500">investment group</h5>
+        </div>
+        <ul
+          className={`flex sm:flex-row sm:absolute lg:relative sm:top-20 lg:top-0 sm:left-0 py-4 lg:py-0 sm:bg-white lg:bg-inherit sm:justify-around sm:w-full lg:w-fit ${!menu ? "hidden" : "flex-col"
+            } lg:flex md:flex-row justify-between md:items-center gap-3 md:gap-5 w-fit`}
+          onClick={() => setMenu(false)}
+        >
+          <NavLink to="/"><li>home</li></NavLink>
+          <NavLink to="/about-us" className="hover:text-[#FF450D]">
+            <li>about</li>
+          </NavLink>
+          <NavLink to="/contact-us" className="hover:text-[#FF450D]">
+            <li>contact us</li>
+          </NavLink>
+
+        </ul>
+        <ul
+          className={`${!menu ? "min-[360px]:absolute sm:relative min-[360px]:right-14 sm:right-0 max-[447px]:hidden" : ""
+            }  flex sm:flex-row justify-between sm:items-center gap-2 md:gap-5 w-fit`}
+        >
+          {!address && <>
+            <button className="hover:bg-orangePrimary bg-white hover:text-white border-2 text-orangeText border-orangeText hover:border-transparent transition-all duration-500 ease-in-out rounded-md px-2 py-1 sm:px-4 sm:py-2 capitalize font-LexendDeca font-normal w-fit">
+              <span className="block absolute w-full h-full left-0 top-0 opacity-0"><ConnectButton chainStatus="icon" /></span>
+              <span>Connect Wallet</span>
+            </button>
+          </>
+          }
+          {address && <>
+            <button className="hover:bg-orangePrimary bg-white hover:text-white border-2 text-orangeText border-orangeText hover:border-transparent transition-all duration-500 ease-in-out rounded-md px-2 py-1 sm:px-7 sm:py-2 capitalize font-LexendDeca font-normal" onClick={() => { modalHandler() }}>create budget</button>
+            <ConnectButton chainStatus="icon" />
+          </>}
+        </ul>
+      </nav>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,44 +2,69 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Vote from './pages/vote.jsx';
 import Budget from './pages/Budget.jsx';
 import AboutUs from './pages/AboutUs.jsx';
 import ContactUs from './pages/ContactUs.jsx';
-import  WalletProvider  from './contex/WalletContextProvider.jsx';
+import WalletProvider from './contex/WalletContextProvider.jsx';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { rootstockTestnet } from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: 'Fineace',
+  projectId: 'fc46a95e1edcd6b90af30fe257ab28be',
+  chains: [rootstockTestnet],
+  ssr: false, // If your dApp uses server side rendering (SSR)
+});
 
 const root = createBrowserRouter([
   {
-    path:"/",
+    path: "/",
     element: <App />,
     children: [
       {
-        path:"/",
+        path: "/",
         element: <Home />,
       },
     ],
   },
   {
-    path:"/about-us",
+    path: "/about-us",
     element: <AboutUs />,
   },
   {
-    path:"/budget",
+    path: "/budget",
     element: <Budget />,
   },
   {
-    path:"/contact-us",
+    path: "/contact-us",
     element: <ContactUs />,
   },
 ]);
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <WalletProvider>
-      <RouterProvider router={root}/>
-    </WalletProvider>
-  
-  </StrictMode>,
+  <WagmiProvider config={config}>
+    <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>
+        <StrictMode>
+          <WalletProvider>
+            <RouterProvider router={root} />
+          </WalletProvider>
+        </StrictMode>
+      </RainbowKitProvider>
+    </QueryClientProvider>
+  </WagmiProvider>
 )
